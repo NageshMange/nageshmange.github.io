a670af84c38d250b7462d2026b9c4b266aa75dc9
NageshMange <mangenagesh@gmail.com>
2025-04-04 23:55:03 +0530

    improvement to site

--- script.js
+++ script.js
@@ -1,44 +1,36 @@
-function hamburg(){
-    const navbar = document.querySelector(".dropdown")
-    navbar.style.transform = "translateY(0px)"
-}
-function cancel(){
-    const navbar = document.querySelector(".dropdown")
-    navbar.style.transform = "translateY(-500px)"
-}
-// Typewriter Effect
-const texts = [
-    "STUDENT",
-    "DEVELOPER",
-    "TECH ENTHUSIAST",
-]
-let speed  =100;
-const textElements = document.querySelector(".typewriter-text");
-let textIndex = 0;
-let charcterIndex = 0;
-function typeWriter(){
-    if (charcterIndex < texts[textIndex].length){
-        textElements.innerHTML += texts[textIndex].charAt(charcterIndex);
-        charcterIndex++;
-        setTimeout(typeWriter, speed);
-    }
-    else{
-        setTimeout(eraseText, 1000)
-    }
-}
-function eraseText(){
-    if(textElements.innerHTML.length > 0){
-        textElements.innerHTML = textElements.innerHTML.slice(0,-1);
-        setTimeout(eraseText, 50)
-    }
-    else{
-        textIndex = (textIndex + 1) % texts.length;
-        charcterIndex = 0;
-        setTimeout(typeWriter, 500)
-    }
-}
-window.onload = typeWriter
+// Mobile menu toggle
 function toggleMenu() {
-    let menu = document.querySelector(".dropdown");
-    menu.style.left = menu.style.left === "0px" ? "-100%" : "0px";
-}
+    const menu = document.getElementById("mobile-menu");
+    menu.classList.toggle("menu-active");
+  }
+  
+  // Typewriter effect
+  const texts = ["STUDENT", "DEVELOPER", "TECH ENTHUSIAST"];
+  const speed = 100;
+  const textElement = document.querySelector(".typewriter-text");
+  let textIndex = 0;
+  let charIndex = 0;
+  
+  function typeWriter() {
+    if (charIndex < texts[textIndex].length) {
+      textElement.innerHTML += texts[textIndex].charAt(charIndex);
+      charIndex++;
+      setTimeout(typeWriter, speed);
+    } else {
+      setTimeout(eraseText, 1000);
+    }
+  }
+  
+  function eraseText() {
+    if (textElement.innerHTML.length > 0) {
+      textElement.innerHTML = textElement.innerHTML.slice(0, -1);
+      setTimeout(eraseText, 50);
+    } else {
+      textIndex = (textIndex + 1) % texts.length;
+      charIndex = 0;
+      setTimeout(typeWriter, 500);
+    }
+  }
+  
+  window.onload = typeWriter;
+  
